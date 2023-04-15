ea126166e86096e2e1d56303a066ba4ff3f30735
Anwar Minarso <anwarminarso@users.noreply.github.com>
2023-04-16 01:35:27 +0700

    Update esp32LapTimer.ino

--- Rewrite/esp32LapTimer/esp32LapTimer.ino
+++ Rewrite/esp32LapTimer/esp32LapTimer.ino
@@ -1,3 +1,19 @@
+//------------------------------------------------------------------
+ // Copyright(c) 2023 a2n Technology
+ // Anwar Minarso (anwar.minarso@gmail.com)
+ // https://github.com/anwarminarso/
+ // This file is part of the Mini4WDLapTimer project.
+ //
+ // This library is free software; you can redistribute it and/or
+ // modify it under the terms of the GNU Lesser General Public
+ // License as published by the Free Software Foundation; either
+ // version 2.1 of the License, or (at your option) any later version.
+ //
+ // This library is distributed in the hope that it will be useful,
+ // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+ // Lesser General Public License for more details
+ //------------------------------------------------------------------
 
 #define TotalLanes 3
 #define Timer_Render_Delay 10 // 10 milisecond
