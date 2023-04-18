427ba6854a9aeccc1728dccf8b8f14fb50433ac1
Anwar Minarso <anwarminarso@users.noreply.github.com>
2023-04-19 00:29:07 +0700

    Update esp32LapTimer.ino

--- Rewrite/esp32LapTimer/esp32LapTimer.ino
+++ Rewrite/esp32LapTimer/esp32LapTimer.ino
@@ -19,6 +19,7 @@
 
 #define TotalLanes 3
 
+
 /*
 1000 milisecond = 1 second
 1000 ms == 1 s
@@ -29,12 +30,14 @@
 
 #define Timer_Digit 4 // 4 digit
 
+
+
 //Pin connected to latch pin (ST_CP) of 74HC595
-const int latchPin = 8;
+const int latchPin = 5;
 //Pin connected to clock pin (SH_CP) of 74HC595
-const int clockPin = 12;
+const int clockPin = 18;
 //Pin connected to Data in (DS) of 74HC595
-const int dataPin = 11;
+const int dataPin = 19;
 
 uint8_t segmentCodes[Timer_Digit] = { 0, 0, 0, 0 };
 ShiftRegister74HC595<Timer_Digit> sr(dataPin, clockPin, latchPin);
@@ -54,25 +57,43 @@ uint8_t numbersCode[10] = {
 uint8_t inverseNumbersCode[10] = {
 	B11000000, //0
 	B11111001, //1
-	B11100100, //2
-	B10110000, //3
-	B10011001, //4
-	B10010010, //5
-	B10000010, //6
+	B01100100, //2
+	B01110000, //3
+	B01011001, //4
+	B01010010, //5
+	B01000010, //6
 	B11111000, //7
-	B10000000, //8
-	B10010000 //9
+	B01000000, //8
+	B01010000 //9
 };
 
 
-// 3 Lane Limit Swich
-volatile uint8_t laneIndex[TotalLanes] = { 0, 1, 2 };
-volatile uint8_t limitSwitchPins[TotalLanes] = { 12, 13, 14 };
-// 3 Lane LED
-volatile uint8_t ledLane[TotalLanes] = { 5, 6, 7};
+/*
+32 Ready Button
+33 Final Lap Button
+25 Limit Switch Lane A
+26 Limit Switch Lane B
+27 Limit Switch Lane C
+14 LED Lane A
+12 LED Lane B
+13 LED Lane C
+26 LED A
+25 LED B
+33 LED C
+32 LS A
+35 LS B
+34 LS C
+39 Ready
+36 Final Lap / Reset
+*/
 
-volatile uint8_t readyButtonPin = 3;
-volatile uint8_t finalLapButtonPin = 4;
+
+//readyButton, finalButton, Lane A, Lane B, Lane C
+uint8_t inputPins[2 + TotalLanes] = { 32, 33, 25, 26, 27 };
+uint8_t ledLane[TotalLanes] = { 14, 12, 13 };
+
+// 3 Lane Limit Swich
+uint8_t laneIndex[TotalLanes] = { 0, 1, 2 };
 
 enum States {
 	Ready,
@@ -81,116 +102,154 @@ enum States {
 	Finished
 };
 
-volatile States currentState = States::Ready;
-volatile uint32_t startTime;
-volatile uint32_t timer;
-volatile uint32_t timer_delay;
-volatile uint32_t last_timer_delay;
+States currentState = States::Ready;
+uint32_t startTime;
+uint32_t timer;
+uint32_t timer_delay;
+uint32_t last_timer_delay;
+bool flipflop = false;
 
 void ready() {
 	currentState = States::Ready;
-	for (int8_t i = 0; i < TotalLanes; i++)
+	for (int8_t i = 0; i < TotalLanes + 2; i++)
 	{
-		uint8_t idx = laneIndex[i];
-		digitalWrite(ledLane[i], LOW); // turn off led
-		attachInterruptArg(limitSwitchPins[i], &LimitSwitch_ISR, &idx, FALLING); // enable limit switch trigger
+		pinMode(inputPins[0], INPUT_PULLUP);
 	}
 	timer_delay = 0;
 	last_timer_delay = 0;
 	timer = 0;
 	renderTimer();
+	//Serial.println("Ready");
 }
 void started(uint8_t laneIdx) {
-	for (int8_t i = 0; i < TotalLanes; i++)
-		detachInterrupt(limitSwitchPins[i]); // disable limit switch trigger
 	startTime = millis(); // init current timestamp
+	currentState = States::Started;
+	//Serial.println("Started");
+}
+void cancelFinalLap() {
+	for (int8_t i = 0; i < TotalLanes; i++)
+	{
+		digitalWrite(ledLane[i], LOW); // turn on led
+	}
 	currentState = States::Started;
 }
 void finalLap() {
 	for (int8_t i = 0; i < TotalLanes; i++)
 	{
-		uint8_t idx = laneIndex[i];
-		attachInterruptArg(limitSwitchPins[i], &LimitSwitch_ISR, &idx, FALLING); // enable limit switch trigger
 		digitalWrite(ledLane[i], HIGH); // turn on led
 	}
 	currentState = States::FinalLap;
+	//Serial.println("Final Lap");
 }
 void finished(uint8_t laneIdx) {
 	currentState = States::Finished;
 	for (int8_t i = 0; i < TotalLanes; i++) 
 	{
-		detachInterrupt(limitSwitchPins[i]); // disable limit switch trigger
 		if (laneIdx != i)
 			digitalWrite(ledLane[i], LOW);  // turn off led, except finished lane
 	}
-}
-
-// Limit Switch Trigger
-void IRAM_ATTR LimitSwitch_ISR(void *arg) {
-	uint8_t* ptr = static_cast<uint8_t*>(arg);
-	uint8_t laneIdx = *ptr;
-	
-	switch (currentState)
-	{
-		case States::Ready:
-			started(laneIdx);
-			break;
-		case States::FinalLap:
-			finished(laneIdx);
-			break;
-		default:
-			return;
-	}
-}
-
-// Ready Button Trigger
-void IRAM_ATTR ReadyButton_ISR() {
-	if (currentState != States::FinalLap && currentState != States::Finished)
-		return;
-	ready();
-}
-
-// Final Lap Button Trigger
-void IRAM_ATTR FinalLapButton_ISR() {
-	if (currentState != States::Started)
-		return;
-	finalLap();
+	//Serial.println("Finished");
 }
 
 // logic Render Timer Display
 void renderTimer() {
+	flipflop = ((timer / 100) % 10) < 5;
 	for (uint8_t i = 0; i < Timer_Digit; i++)
 	{
-		uint32_t div = pow10(i);
+		uint32_t div = pow10(i) * 10;
 		uint8_t digitNumber = (timer / div) % 10;
-		if (i != 2)
-			segmentCodes[i] = numbersCode[digitNumber];
+		uint8_t invIdx = Timer_Digit - i - 1;
+
+		if (i != 1)
+		{
+			segmentCodes[invIdx] = numbersCode[digitNumber];
+			if (i == 2 && flipflop)
+				segmentCodes[invIdx] &= ~(1 << 7);
+		}
 		else
-			segmentCodes[i] = inverseNumbersCode[digitNumber];
+		{
+			segmentCodes[invIdx] = inverseNumbersCode[digitNumber];
+			if (flipflop)
+				segmentCodes[invIdx] &= ~(1 << 6);
+		}
 	}
 	sr.setAll(segmentCodes);
 }
 
+void renderFinishedTimer() {
+	segmentCodes[1] &= ~(1 << 6);
+	segmentCodes[2] &= ~(1 << 7);
+	sr.setAll(segmentCodes);
+}
 void setup() {
+	//Serial.begin(115200);
+
+	for (int8_t i = 0; i < sizeof(inputPins); i++)
+	{
+		pinMode(inputPins[i], INPUT_PULLUP);
+	}
 	for (int8_t i = 0; i < TotalLanes; i++)
 	{
-		pinMode(limitSwitchPins[i], INPUT_PULLUP);
 		pinMode(ledLane[i], OUTPUT);
 	}
-	attachInterrupt(readyButtonPin, &ReadyButton_ISR, FALLING); // enable ready button trigger
-	attachInterrupt(finalLapButtonPin, &FinalLapButton_ISR, FALLING); // enable final lap button trigger
 	ready();
 }
 
 void loop() {
-	if (currentState != States::Started && currentState != States::FinalLap)
-		return;
-
-	timer = millis() - startTime;
-	timer_delay = timer / Timer_Render_Delay;
-	if (timer_delay - last_timer_delay > 0)
+	
+	switch (currentState)
 	{
-		last_timer_delay = timer_delay;
-		renderTimer();
+		case States::Ready:
+			{
+				for (uint8_t i = 2; i < sizeof(inputPins); i++)
+				{
+					if (digitalRead(inputPins[i]) == LOW)
+					{
+						started(i - 2);
+						break;
+					}
+				}
+			}
+			break;
+		case States::Started:
+			{
+				if (digitalRead(inputPins[1]) == LOW)
+					finalLap();
+			}
+			break;
+		case States::FinalLap:
+			{
+				if (digitalRead(inputPins[0]) == LOW)
+				{
+					cancelFinalLap();
+				} 
+				else 
+				{
+					for (uint8_t i = 2; i < sizeof(inputPins); i++)
+					{
+						if (digitalRead(inputPins[i]) == LOW)
+						{
+							finished(i - 2);
+							break;
+						}
+					}
+				}
+			}
+			break;
+		case States::Finished:
+			{
+				if (digitalRead(inputPins[0]) == LOW)
+					ready();
+			}
+			break;
+	}
+	if (currentState == States::Started || currentState == States::FinalLap) {
+		timer = millis() - startTime;
+		timer_delay = timer / Timer_Render_Delay;
+		if (timer_delay - last_timer_delay > 0)
+		{
+			last_timer_delay = timer_delay;
+			renderTimer();
+		}
 	}
 }
